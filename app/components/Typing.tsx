0c8900bc504e70b3cbf9a08f0db13eae087788bc
Rahul Khushalani <rahulkhushalani@proton.me>
2024-04-25 13:20:30 +0530

    folder structure changed

---
+++ app/components/Typing.tsx
@@ -0,0 +1,93 @@
+"use client"
+import React, { useState, useEffect, useRef } from 'react';
+
+const TypingTest = () => {
+    const cursorRef = useRef<HTMLSpanElement>(null);
+    const scrollRef = useRef<HTMLDivElement>(null);
+    const [state, setState] = useState<{ wordIndex: number, letterIndex: number, completedWords: string[], typedWords: string[] }>({ wordIndex: 0, letterIndex: 0, completedWords: [], typedWords: [] });
+    const paragraph = "Everything that you thought had meaning: every hope, dream, or moment of happiness. None of it matters as you lie bleeding out on the battlefield. None of it changes what a speeding rock does to a body, we all die.But does that mean our lives are meaningless?... Does that mean that there was no point in our being born?... Would you say that of our slain comrades?... What about their lives?... Were they meaningless?..They were not! Their memory serves as an example to us all! The courageous fallen! The anguished fallen! Their lives have meaning because we the living refuse to forget them!And as we ride to certain death, we trust our successors to do the same for us! Because my soldiers do not buckle or yield when faced with the cruelty of this world! My soldiers push forward! My soldiers scream out! My soldiers RAAAAAGE!";
+
+
+    const unsupportedKeys = ["Shift", "CapsLock", "Control", "Alt", "Tab", "Escape", "Meta", "ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown", "Home", "End", "PageUp", "PageDown", "Insert", "Delete", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "ScrollLock", "Pause"]
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyPress);
+        cursorRef.current?.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' });
+        return () => {
+            window.removeEventListener('keydown', handleKeyPress);
+        };
+
+    }, [state]);
+
+    const handleKeyPress = (e: KeyboardEvent) => {
+        console.log(e.key)
+        const words = paragraph.split(" ");
+        const currentWord = words[state.wordIndex];
+        const currentLetter = currentWord.split("")[state.letterIndex];
+
+        let newTypedWords = [...state.typedWords];
+
+        if (unsupportedKeys.includes(e.key)) return;
+
+        if (e.key === 'Backspace') {
+            if (newTypedWords[state.wordIndex]) {
+                newTypedWords[state.wordIndex] = newTypedWords[state.wordIndex].slice(0, -1);
+            }
+        } else {
+            if (newTypedWords[state.wordIndex]) {
+                newTypedWords[state.wordIndex] += e.key;
+            } else {
+                newTypedWords[state.wordIndex] = e.key;
+            }
+        }
+
+        let newWordIndex = state.wordIndex;
+        let newLetterIndex = state.letterIndex;
+        if (e.key !== 'Backspace') {
+            newLetterIndex++;
+        } else if (newLetterIndex > 0) {
+            newLetterIndex--;
+        }
+
+        let newCompletedWords = [...state.completedWords];
+
+        if (newLetterIndex === currentWord.length) {
+            newWordIndex++;
+            newLetterIndex = 0;
+            newCompletedWords.push(currentWord);
+        }
+
+        setState({ wordIndex: newWordIndex, letterIndex: newLetterIndex, completedWords: newCompletedWords, typedWords: newTypedWords });
+    };
+    return (
+        <div className="flex justify-center h-screen items-center bg-[rgb(246,246,247)]">
+            <div className='w-screen p-12'>
+                <div
+                    onClick={(e) => {
+
+                    }}
+                    ref={scrollRef} className='shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(0,0,0,0.1)] rounded-lg  h-36 overflow-x-auto scrollbar-hidden flex items-center justify-center'>
+                    <div className='inline-flex items-center justify-center transform translate-x-1/2'>
+                        {paragraph.split(" ").map((word, wordIndex) => {
+                            return (
+                                <span key={wordIndex} className="m-1">
+                                    {word.split("").map((letter, letterIndex) => {
+                                        return (
+                                            <>
+                                                {wordIndex === state.wordIndex && letterIndex === state.letterIndex && <span
+                                                    ref={cursorRef}
+                                                    className="cursor text-4xl">|</span>}
+                                                <span key={letterIndex} className={`text-3xl font-light ${wordIndex === state.wordIndex && letterIndex === state.letterIndex ? "text-blue-700" : wordIndex < state.wordIndex ? (word === state.typedWords[wordIndex] ? "text-green-500" : "text-red-500") : "text-black"}`}>{letter}</span>
+                                            </>
+                                        );
+                                    })}
+                                </span>
+                            );
+                        })}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TypingTest;
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import TypingTest from "./components/Typing";
+
+
+
+
+export default function Home() {
+return(
+  <TypingTest />
+)
+}
